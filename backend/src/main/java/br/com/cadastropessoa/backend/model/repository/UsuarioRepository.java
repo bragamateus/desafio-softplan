c9980530654b52816fa2085bfe4ca4a273d58cfd
Mateus Braga <braga.mateus16@gmail.com>
2021-01-29 00:24:17 -0300

    Antenticação com OAuth2 e JWT

--- backend/src/main/java/br/com/cadastropessoa/backend/model/repository/UsuarioRepository.java
+++ backend/src/main/java/br/com/cadastropessoa/backend/model/repository/UsuarioRepository.java
@@ -3,6 +3,9 @@ package br.com.cadastropessoa.backend.model.repository;
 import br.com.cadastropessoa.backend.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsername(String username);
 
 }
