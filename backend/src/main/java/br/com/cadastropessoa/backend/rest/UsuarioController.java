7e3c12e32bc754916bcd5f6dc8e8ac67cd5e2eed
Mateus Braga <braga.mateus16@gmail.com>
2021-01-28 21:40:57 -0300

    modela entidade usuario

---
+++ backend/src/main/java/br/com/cadastropessoa/backend/rest/UsuarioController.java
@@ -0,0 +1,27 @@
+package br.com.cadastropessoa.backend.rest;
+
+import br.com.cadastropessoa.backend.model.entity.Usuario;
+import br.com.cadastropessoa.backend.model.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void salvar(@RequestBody @Valid Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+}
